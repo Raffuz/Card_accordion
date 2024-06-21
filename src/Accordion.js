719f089a93bee32d3522cd9c655d9f3a013b042b
Raffuz <raffaele11596@gmail.com>
2024-06-21 22:58:57 +0200

    Fix:_Card

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import AccordionItem from "./AccordionItem";
 
 export default function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
+
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="test 1"
+        num={22}
+        key="test 1"
+      >
+        <p>ciaooooooooooooo</p>
+        <ul>
+          <li>ciao</li>
+          <li>ciao</li>
+          <li>ciao</li>
+          <li>ciao</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
